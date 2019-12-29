67b6a2f084de8bedc3d471c0882081f118c64dc0
Leah Dodson <leahdodson9@gmail.com>
2019-12-28 23:59:11 -0600

    added debugging elements and defined more variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@
 var wordList = [
     "snowflake",
     "present",
-    "candycane",
     "santa",
     "ornaments",
     "sleigh",
@@ -17,6 +16,7 @@ var wordList = [
 //!Select a random word from the wordList array
 var wordRandom = wordList[Math.floor(Math.random() * wordList.length)];
 // var wordSelected = wordRandom.split("");
+alert(wordRandom)
 
 //!Creates an array from the selected word and displays __ in the HTML for each letter in the word
 var answerArray = [];
@@ -29,27 +29,40 @@ for (var i = 0; i < wordRandom.length; i++) {
 
 //!Set game stats
 var guessesLeft = 10;
-document.getElementById("guessesLeft").innerHTML = guessesLeft
-var lettersGuessed = "";
-document.getElementById("lettersGuessed").innerHTML = lettersGuessed
+document.getElementById("guessesLeft").innerHTML = guessesLeft;
+var lettersGuessed = [];
+document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+var wins = 0;
+document.getElementById("totalWins").innerHTML = wins;
+
 
 var remainingLetters = wordRandom.length;
-// while (remainingLetters > 0) {
+//!Collects user input
 document.onkeyup = function (event) {
     var userGuess = event.key
     alert(userGuess);
+
+    //!Checks that the userGuess exists in the word array
+    // while (remainingLetters > 0) {
     for (var j = 0; j < wordRandom.length; j++) {
-        if (wordRandom[j] === userGuess) {
+        if (wordRandom[j] === userGuess && remainingLetters > 0) {
             answerArray[j] = userGuess;
             remainingLetters--;
+            console.log(remainingLetters)
+            console.log(answerArray)
+            document.getElementById("word").innerHTML = answerArray.join(" ");
         }
+        // else if (remainingLetters = 0) {
+        //     alert("You Win!");
+        //     document.getElementsByClassName("wins").innerHTML = wins + 1
+
     }
-    // }
-
-
-    //!Game code
-
-    //!Update stats
-    //!keep collecting input
-    //!Update answerArray and remainingLetters
 }
+
+
+
+//!Game code
+
+//!Update stats
+//!keep collecting input
+//!Update answerArray and remainingLetters

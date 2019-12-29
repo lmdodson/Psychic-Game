5adca467b186f53fdd402a558459f01ccba4ec3f
Leah Dodson <leahdodson9@gmail.com>
2019-12-29 17:57:05 -0600

    changed letter checking function and added more variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,6 +23,10 @@ var wordLetters = [];
 var lettersGuessed = [];
 
 var answerArray = [];
+// letter that the player
+var userGuess = "";
+
+var wordSpaces = 0;
 
 //Stats variables
 var guessesLeft = 10;
@@ -31,24 +35,31 @@ var losses = 0;
 
 //!Functions
 
+// sets intial stats in html
 function setStats() {
     document.getElementById("guessesLeft").innerHTML = guessesLeft;
     document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
     document.getElementById("totalWins").innerHTML = wins;
 }
 
+// reset function
+function reset() {
+    guessesLeft = 10;
+    lettersGuessed = [];
+    answerArray = [];
+    wordSelection();
+}
 
+// runs word selection and placement into html
 function wordSelection() {
-
     //Select a random word from the wordList array
     wordRandom = wordList[Math.floor(Math.random() * wordList.length)];
-
     //split the word into new array
     wordLetters = wordRandom.split("");
-
+    // stores the length of the current word as a number
+    wordSpaces = wordLetters.length;
     // for testing purposes
     alert(wordRandom)
-
     //Creates an array from the selected word and displays __ in the HTML for each letter in the word
     for (var i = 0; i < wordLetters.length; i++) {
         answerArray[i] = "  __  ";
@@ -61,27 +72,45 @@ setStats()
 wordSelection()
 // alert(wordRandom)
 
-
-
-
-var remainingLetters = wordRandom.length;
-//!Collects user input
-document.onkeyup = function (event) {
-    var userGuess = event.key
-    alert(userGuess);
-
-    //!Checks that the userGuess exists in the word array
-    // while (remainingLetters > 0) {
-    for (var j = 0; j < wordRandom.length; j++) {
-        if (wordRandom[j] === userGuess && remainingLetters > 0) {
-            answerArray[j] = userGuess;
-            remainingLetters--;
-            console.log(remainingLetters)
-            console.log(answerArray)
-            document.getElementById("word").innerHTML = answerArray.join(" ");
+function checkLetters(letter) {
+    var letterPresent = false;
+    for (var i = 0; i < wordSpaces; i++) {
+        if (wordRandom[i] == letter) {
+            letterPresent = true;
         }
     }
+    if (letterPresent) {
+        for (var i = 0; i < wordSpaces; i++) {
+            if (wordRandom[i] == letter) {
+                answerArray[i] == letter;
+            }
+        }
+
+    } else {
+        lettersGuessed.push(letter);
+        guessesLeft--;
+    }
 }
+
+
+// var remainingLetters = wordRandom.length;
+// //!Collects user input
+// document.onkeyup = function (event) {
+//     var userGuess = event.key
+//     alert(userGuess);
+
+//     //!Checks that the userGuess exists in the word array
+//     // while (remainingLetters > 0) {
+//     for (var j = 0; j < wordRandom.length; j++) {
+//         if (wordRandom[j] === userGuess && remainingLetters > 0) {
+//             answerArray[j] = userGuess;
+//             remainingLetters--;
+//             console.log(remainingLetters)
+//             console.log(answerArray)
+//             document.getElementById("word").innerHTML = answerArray.join(" ");
+//         }
+//     }
+// }
 
 
 //!Game code

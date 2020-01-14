6cca6ac68c12244560f7316c45ce347884e33c62
Leah Dodson <leahdodson9@gmail.com>
2020-01-14 00:02:58 -0600

    changed background img, deleted tree, removed debugging lines and excess comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ var wordList = [
     "cookies",
     "frosty",
     "lights",
-    "cuddles",
+    "caroling",
     "eggnog",
     "grinch",
     "mistletoe",
@@ -61,10 +61,7 @@ function wordSelection() {
     wordRandom = wordList[Math.floor(Math.random() * wordList.length)];
     //split the word into new array
     wordLetters = wordRandom.split("");
-    // stores the length of the current word as a number
-    // wordSpaces = wordLetters.length;
-    // for testing purposes
-    // alert(wordRandom)
+
     //Creates an array from the selected word and displays __ in the HTML for each letter in the word
     for (var i = 0; i < wordLetters.length; i++) {
         answerArray[i] = "  __  ";
@@ -86,9 +83,6 @@ function game() {
         const userGuess = event.key.toLowerCase();
         //add the guess to the letters Guessed array
         lettersGuessed.push(userGuess);
-        //for debugging
-        //console.log(userGuess);
-        //alert(wordLetters);
 
         //Ensures user has guesses left then tests if guess is correct or incorrect
         if (lettersGuessed.length < 10) {
@@ -96,42 +90,34 @@ function game() {
             if (wordRandom.match(userGuess)) {
                 //removes the letter from the array because it's right
                 lettersGuessed.pop(userGuess)
-                // alert(userGuess + (" IS in the word: " + wordRandom + "\nNumber of guesses used: " + lettersGuessed.length + "/10"));
                 //finds where in the array the letter is
                 for (var j = 0; j <= wordLetters.length; j++) {
                     if (wordRandom[j] === userGuess) {
                         answerArray[j] = userGuess;
-                        //alert("for loop before doc");
                         //adds the correct letter inside all occurances in html
                         document.getElementById("word").innerHTML = answerArray.join(" ");
-                        // alert("for loop after doc");
                     }
                 }
                 //check if the word is solved
                 if (wordLetters.toString() == answerArray.toString()) {
-                    //alert("before wins++");
                     wins++
                     //add win to html
                     document.getElementById("totalWins").innerHTML = wins;
-                    // alert ("You win! \nNext word selected. Choose a letter to keep playing");
                     alert("Congratulations you won! Choose another letter to keep playing!")
                     //start a new game
                     reset();
                 }
             } else {
-                //wrong guess
                 //adds wrong guess to html
                 document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
                 //counts down guesses left
                 guessesLeft--
                 //updates number of guessesLeft in html
                 document.getElementById("guessesLeft").innerHTML = guessesLeft;
-                // alert(userGuess + (" IS NOT in the word: " + wordRandom + "\nNumber of guesses used: " + lettersGuessed.length + "/10"));
             }
         } else {
             //Game Over
             ++losses
-            // alert("Out of guesses! \nNext word selected. Choose a letter to keep playing!");
 
             alert("You lost! The word was " + wordRandom);
             //start a new game
@@ -144,7 +130,6 @@ function game() {
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
     console.log('content loaded');
-    //set stats
     setStats();
     wordSelection();
     game();
